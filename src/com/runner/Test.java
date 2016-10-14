03febb6f519babe5dcff7a53f89fc4201f47be4b
wavesea <lhz871102@163.com>
2016-10-14 14:35:08 +0800

    add

---
+++ src/com/runner/Test.java
@@ -0,0 +1,20 @@
+package com.runner;
+
+/**
+ * 
+ * @author LiHaibo
+ *
+ * @fileName Test.java
+ * @args 
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello,world");
+		System.out.println("runner is Company...");
+		
+	}
+
+}
